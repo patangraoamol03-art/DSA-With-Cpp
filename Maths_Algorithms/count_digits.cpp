4dd5037f574a44502feae90c563f650c0f867782
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-06 16:50:33 +0530

    Add count_digits.cpp to count number of digits

---
+++ Maths_Algorithms/count_digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int count = 0;
+    while (n > 0) {
+        n /= 10;
+        count++;
+    }
+
+    cout << "\nNumber of digits are:\n" << count;
+    return 0;
+}
