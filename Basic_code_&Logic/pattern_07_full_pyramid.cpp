540e04a343188d525f5b2b569e4d0e00798cb8c7
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-06 16:08:10 +0530

    Add full pyramid pattern printing program
    This program prompts the user for the number of rows and prints a full pyramid pattern using asterisks.

---
+++ Basic_code_&Logic/pattern_07_full_pyramid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n ;
+    cout << "Enter a Number of rows: -" ;
+    cin >> n ;
+    for (int i = 1; i <= n ; i++) {
+       for (int j = 0 ; j <= n -i - 1 ; j++){
+           cout << ' ' ;
+       }
+    for (int j = 1 ; j <= 2*i -1 ; j++){
+           cout << "*" ;
+       }
+    for (int j = 1 ; j <= n -i ; j++){
+           cout << ' ' ;
+       }
+       cout << endl ;
+    }
+    return 0;
+}
