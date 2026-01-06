4884b20062710c80031512d046268216d33113b2
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-06 16:25:22 +0530

    Add pattern to print repeated alphabet triangle

---
+++ Basic_code_&Logic/pattern_16_repeated_alphabet_triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    char alpha = 'A';
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << alpha << " ";
+        }
+        alpha++;
+        cout << endl;
+    }
+    return 0;
+}
