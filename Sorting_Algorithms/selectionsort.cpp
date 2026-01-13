d2be4d1503f0577a89fb27a0314b14006b7902bf
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-13 07:36:08 +0530

    Update selectionsort.cpp

--- Sorting_Algorithms/selectionsort.cpp
+++ Sorting_Algorithms/selectionsort.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void SelectionSort(int arr[],int n){
-     for (int i = 0; i < n - 1; i++) {
+void SelectionSort(vector<int>& arr, int n) {
+  
+
+    for(int i = 0; i < n - 1; i++) {
         int minIndex = i;
 
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
 
-        if (minIndex != i) {
+        if(minIndex != i) {
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
     }
-
 }
+
 int main() {
     int n;
     cout << "Enter your size of an array: ";
     cin >> n;
 
-    int arr[n];
+    vector<int> arr(n);
 
     cout << "Enter elements of an array:\n";
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-   
+
     SelectionSort(arr,n);
-   
+
     cout << "Sorted array:\n";
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cout << arr[i] << "\n";
     }
 
