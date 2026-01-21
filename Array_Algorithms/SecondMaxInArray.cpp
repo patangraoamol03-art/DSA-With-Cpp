1c0f82f9ebd574a4a7cfcab1a571589d5a8641f3
patangraoamol03-art <patangrao.amol03@gmail.com>
2026-01-21 22:58:19 +0530

    Create SecondMaxInArray.cpp

---
+++ Array_Algorithms/SecondMaxInArray.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter elements of the array:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    int secondMaxVal = INT_MIN;
+    int secondMaxIndex = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != maxVal && arr[i] > secondMaxVal) {
+            secondMaxVal = arr[i];
+            secondMaxIndex = i;
+        }
+    }
+
+    if (secondMaxVal == INT_MIN) {
+        cout << "Second maximum does not exist\n";
+    } else {
+        cout << "Second maximum number in the array is " 
+             << secondMaxVal << " at index " << secondMaxIndex << endl;
+    }
+
+    return 0;
+}
